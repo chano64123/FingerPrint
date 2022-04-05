dba4cb5781db7b14a3c8bee054fd239883e7442a
chano64123 <promo201674@gmail.com>
2022-04-05 11:40:26 -0500

    layouts

--- app/src/main/java/com/chano/fingerprint/MainActivity.java
+++ app/src/main/java/com/chano/fingerprint/MainActivity.java
@@ -3,12 +3,22 @@ package com.chano.fingerprint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.chano.fingerprint.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    //binding
+    private ActivityMainBinding binding;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
         setContentView(R.layout.activity_main);
+
+        
     }
 }
