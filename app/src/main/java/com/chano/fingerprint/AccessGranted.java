460f12db98be23d2f5636fdb2eea2476e73cdbd1
chano64123 <promo201674@gmail.com>
2022-04-05 10:57:02 -0500

    recursos

---
+++ app/src/main/java/com/chano/fingerprint/AccessGranted.java
@@ -0,0 +1,14 @@
+package com.chano.fingerprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccessGranted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access_granted);
+    }
+}
